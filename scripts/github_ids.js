e12384fb17f5a7fc9465b615227c9d24372f3f44
jessehartloff <hartloff@buffalo.edu>
2017-09-10 23:00:25 -0400

    add min.txt

--- scripts/github_ids.js
+++ scripts/github_ids.js
@@ -1,3 +1,4 @@
+
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/education');
@@ -5,16 +6,22 @@ var collection = db.get('users');
 
 
 collection.find({"courses_enrolled.cse442-f17.course_key": "cse442-f17"}, {}, function (err, records) {
+	var did_their_shit = [];
 	var ids = [];
 	for (var i in records) {
 		var id = records[i]["courses_enrolled"]["cse442-f17"]["options"]["github_username"]["value"];
 		if(id === ""){
 			continue;
 		}
+		if(did_their_shit.indexOf(id) !== -1){
+			continue;
+		}
 		if (ids.indexOf(id) === -1) {
 			ids.push(id);
+			console.log(id);
 		}
 	}
-	process.stdout.write(ids.toString());
+	console.log(ids);
 	console.log(ids.length);
 });
+
