5f1ed83370667b990bdee3a9f5f2d9fe977e0459
jessehartloff <hartloff@buffalo.edu>
2017-10-26 07:51:39 -0400

    getting logs

--- scripts/webhookLogs.js
+++ scripts/webhookLogs.js
@@ -2,9 +2,13 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/education');
 var collection = db.get('github_activity');
+var fs = require('fs');
 
 
 collection.find({}, {}, function (err, records) {
-	console.log(records);
+	fs.writeFile('./logs.json', JSON.stringify(records, null, 2), function(err){
+		console.log("done");
+	});
+	//console.log(records);
 });
 
