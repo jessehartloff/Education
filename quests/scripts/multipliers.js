daf79ff4ac34f13c2e6709b4f741d249ea8c445b
jessehartloff <hartloff@buffalo.edu>
2018-05-24 14:05:15 -0400

    add CSE331

--- quests/scripts/multipliers.js
+++ quests/scripts/multipliers.js
@@ -36,11 +36,11 @@ function add_large_multipliers(username, quantity){
 }
 
 
-var people = ['fmdestin', 'ousmanka', 'guangxin', 'wmsaulon', 'blakebue', 'annakobi', 'njceccar', 'wp26', 'jenniech', 'spittaro', 'seanjone', 'sanairac', 'bs84', 'vbaziuk', 'alphonsu', 'jamiesau', 'jchen338', 'rejankow', 'yli55', 'jzheng47', 'szheng32', 'mmmcnaug', 'irclark', 'jalmazan', 'mvciacci', 'btmccann', 'djin3', 'elbiltek', 'abrewer2', 'ifrankli', 'bcastell', 'mpriley', 'ethanhul', 'mmcaccam', 'skylarla', 'averywei', 'ziaochen', 'oscarasb', 'aamcclin', 'vtattyba', 'dszczepa', 'wtmills', 'swreming', 'afcovert', 'temeosky', 'sethschu', 'dm235', 'apszolno', 'dannenro', 'djbrior', 'alexisse', 'anmolpat', 'cullenga', 'usmanhus', 'ibarry2', 'badisben', 'svreid2', 'gtcodign', 'narmeena', 'kendall5', 'mjspagna', 'jeandedi', 'mjolenic', 'susanpau', 'drewfiut', 'bjho', 'aarondie', 'bfrawley', 'afuchs2', 'dmbritta', 'jlvella', 'karencru', 'djwhite5', 'jconoh', 'votreesh', 'ldong7', 'zhang77', 'devonwan', 'hchen63', 'sterlynr', 'vtjutton', 'zdgandro', 'aarondom', 'sli84', 'ancruz', 'dylanmah', 'jlsanton', 'sjlevano', 'weikengf', 'jlau28', 'coyotero', 'gli24', 'epwood', 'yusong', 'sandrash', 'msillah', 'camilare', 'njchorda', 'jlu42', 'dhuang34', 'seanbras', 'cvlada', 'jrwelch2', 'ncolan', 'boyuanch', 'xueweiji', 'dtschill', 'lukekuch', 'rmichel', 'avrilall'];
+var people = [];
 
 for(var i=0; i<people.length; i++){
 	var ubit = people[i];
 	console.log(ubit);
 	add_small_multipliers(ubit, 2);
-	add_large_multipliers(ubit, 2)
+	add_large_multipliers(ubit, 2);
 }
